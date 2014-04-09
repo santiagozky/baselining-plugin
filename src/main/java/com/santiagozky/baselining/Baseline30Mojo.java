1501ef11d0853d2fa5d4614151e8a2923216c6f6
Giovanni Meo <gmeo@cisco.com>
2014-04-09 16:16:17 +0200

    Provide a baseline mojo that can work also with maven 3.0.X
    - The baseline mojo uses the new format of the aether library, the one
    hosted on eclipse foundation, but that is usable only starting from
    maven 3.1.X, now there are quite few projects that would still benefit
    of this mojo but are still at 3.0.X level, so for those project this
    fix clones the BaseLineMojo in BaseLine30Mojo that is triggered by the
    goal "baseline30" so you can still get the benefit of this mojo for
    3.0.X projects by simply using the goal baseline30 instead of
    baseline. Definitively this one could be done more elegantly but not
    being a maven mojo expert this at least gets the ball rolling.
    
    Signed-off-by: Giovanni Meo <gmeo@cisco.com>

---
+++ src/main/java/com/santiagozky/baselining/Baseline30Mojo.java
@@ -0,0 +1,270 @@
+package com.santiagozky.baselining;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.Execute;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.sonatype.aether.RepositorySystem;
+import org.sonatype.aether.RepositorySystemSession;
+import org.sonatype.aether.artifact.Artifact;
+import org.sonatype.aether.util.artifact.DefaultArtifact;
+import org.sonatype.aether.repository.RemoteRepository;
+import org.sonatype.aether.resolution.ArtifactRequest;
+import org.sonatype.aether.resolution.ArtifactResolutionException;
+import org.sonatype.aether.resolution.ArtifactResult;
+import org.sonatype.aether.resolution.VersionRangeRequest;
+import org.sonatype.aether.resolution.VersionRangeResolutionException;
+import org.sonatype.aether.resolution.VersionRangeResult;
+
+import aQute.bnd.differ.Baseline;
+import aQute.bnd.differ.Baseline.BundleInfo;
+import aQute.bnd.differ.Baseline.Info;
+import aQute.bnd.differ.DiffPluginImpl;
+import aQute.bnd.osgi.Jar;
+import aQute.bnd.version.Version;
+
+@Mojo(name = "baseline30", defaultPhase = LifecyclePhase.VERIFY)
+@Execute(phase = LifecyclePhase.VERIFY, goal = "baseline")
+public class Baseline30Mojo extends AbstractMojo {
+	// TODO: how to get the real extension?
+	private static final String EXTENSION = ".jar";
+
+	private static final String ARTIFACT_DESCRIPTION = "groupId:artifactId:[0,version)";
+
+	/**
+	 * if strict is true, the build will fail if a package is on a lower version
+	 * than recommended.
+	 */
+	@Parameter
+	private boolean strict = false;
+
+	/**
+	 * Always show the suggested version of a package. By default it will only
+	 * shows it if the suggested is higher than the actual.
+	 */
+	@Parameter
+	private boolean verbose = false;
+
+	// these should not be modified
+
+	@Parameter(defaultValue = "${project.build.directory}")
+	private String target;
+	@Parameter(defaultValue = "${pom.version}")
+	private String version;
+
+	@Parameter(defaultValue = "${pom.groupId}")
+	private String groupId;
+
+	@Parameter(defaultValue = "${pom.artifactId}")
+	private String name;
+
+	@Parameter(defaultValue = "${project.build.finalName}")
+	private String jarName;
+
+	// aether stuff
+
+	@Component
+	private RepositorySystem repoSystem;
+
+	@Parameter(defaultValue = "${repositorySystemSession}")
+	private RepositorySystemSession repoSession;
+
+	@Parameter(defaultValue = "${project.remoteProjectRepositories}")
+	private List<RemoteRepository> projectRepos;
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
+
+		File oldJar = getLastArtifact();
+		if (oldJar == null) {
+			getLog().warn(
+					"Could not find a previous version artifact to compare.");
+			return;
+		}
+
+		File newJar = getCurrentArtifact();
+
+		getLog().info(
+				String.format("Comparing artifact %s against %s",
+						newJar.getName(), oldJar.getName()));
+		getLog().debug("strict mode is " + strict);
+		getLog().debug("verbose mode is " + verbose);
+		DiffPluginImpl differ = new DiffPluginImpl();
+		Baseline baseline;
+		try {
+			baseline = new Baseline(new SimpleReporter(), differ);
+			Jar old = new Jar(oldJar);
+			Jar current = new Jar(newJar);
+
+			Set<Info> infos;
+			try {
+				infos = baseline.baseline(current, old, null);
+			} catch (Exception e) {
+				throw new MojoExecutionException(
+						"could not calculate generate package differential", e);
+			}
+			for (Info info : infos) {
+				Version v = info.suggestedVersion;
+
+				if (verbose) {
+					getLog().info(
+							String.format(
+									"package %s version changed from %s to %s",
+									info.packageName, info.olderVersion,
+									info.newerVersion));
+					getLog().info(
+							String.format("package %s suggested version is %s",
+									info.packageName, v));
+				}
+				if (info.mismatch) {
+					getLog().error(
+							String.format(
+									"package %s version is incorrect. Should be at least %s",
+									info.packageName, v));
+					if (strict) {
+						throw new MojoFailureException(String.format(
+								"package %s version should be at least %s ",
+								info.packageName, v));
+					}
+
+				}
+				if (info.warning != null && info.warning.length() > 0) {
+					getLog().warn(
+							String.format("package %s : %s", info.packageName,
+									info.warning));
+				}
+
+			}
+			BundleInfo binfo = baseline.getBundleInfo();
+			if (!baseline.getNewerVersion().equals(binfo.suggestedVersion)) {
+				getLog().warn(
+						String.format(
+								"Bundle version is %s, the recommended version is %s",
+								baseline.getNewerVersion(),
+								binfo.suggestedVersion));
+			}else{
+				getLog().info(String.format("Bundle version is %s",baseline.getNewerVersion()));
+			}
+			if (binfo.mismatch) {
+				if (strict) {
+					throw new MojoFailureException("wrong version for artifact");
+				}
+
+			}
+
+		} catch (IOException e) {
+			throw new MojoExecutionException("could not calculate  versions", e);
+		}
+
+	}
+
+	private File getCurrentArtifact() {
+		// new jar comes from the target directory, freshly compiled
+		return new File(target, jarName.concat(EXTENSION));
+	}
+
+	/**
+	 * gets the file for the last released artifact.
+	 *
+	 * @return
+	 * @throws MojoExecutionException
+	 */
+	private File getLastArtifact() throws MojoExecutionException {
+        org.sonatype.aether.version.Version v = getLastVersion();
+		if (v == null) {
+			return null;
+		}
+
+		Artifact artifactQuery = new DefaultArtifact(groupId.concat(":")
+				.concat(name).concat(":").concat(v.toString()));
+		getLog().debug(
+				String.format("looking for artifact %s",
+						artifactQuery.toString()));
+		return getArtifactFile(artifactQuery);
+
+	}
+
+	/**
+	 * gets the file for the artifact at the specified version.
+	 *
+	 * @param version
+	 * @return
+	 * @throws MojoExecutionException
+	 */
+	private File getArtifactFile(Artifact artifactQuery)
+			throws MojoExecutionException {
+
+		ArtifactRequest request = new ArtifactRequest(artifactQuery,
+				projectRepos, null);
+		List<ArtifactRequest> arts = new ArrayList<ArtifactRequest>();
+		arts.add(request);
+		try {
+			ArtifactResult a = repoSystem.resolveArtifact(repoSession, request);
+			return a.getArtifact().getFile();
+		} catch (ArtifactResolutionException e) {
+			throw new MojoExecutionException(
+					"could not resolve artifact to compare with", e);
+		}
+
+	}
+
+	/**
+	 * gets the last version of the current artifact, not including the current
+	 * one.
+	 *
+	 * @return
+	 * @throws MojoExecutionException
+	 */
+    private org.sonatype.aether.version.Version getLastVersion()
+			throws MojoExecutionException {
+
+		// build the artifact description with version range from 0 up to (non
+		// inclusive) current version
+
+		String artifactDescription = ARTIFACT_DESCRIPTION.replace("groupId",
+				groupId);
+		artifactDescription = artifactDescription.replace("artifactId", name);
+		artifactDescription = artifactDescription.replace("version", version);
+
+		Artifact artifact = new DefaultArtifact(artifactDescription);
+		getLog().info(
+				String.format("searching for artifacts in range %s",
+						artifactDescription));
+		VersionRangeRequest rangeRequest = new VersionRangeRequest();
+		rangeRequest.setArtifact(artifact);
+		rangeRequest.setRepositories(projectRepos);
+
+		VersionRangeResult rangeResult;
+		try {
+			rangeResult = repoSystem.resolveVersionRange(repoSession,
+					rangeRequest);
+            List<org.sonatype.aether.version.Version> versions = rangeResult
+					.getVersions();
+			getLog().debug(
+					String.format("found versions %s",
+							rangeResult.getVersions()));
+			// could not find a previous version
+			if (versions.isEmpty()) {
+				return null;
+			}
+            org.sonatype.aether.version.Version lastVersion = versions
+					.get(versions.size() - 1);
+			getLog().debug(String.format("previous version is %s", lastVersion));
+			return lastVersion;
+
+		} catch (VersionRangeResolutionException e) {
+			throw new MojoExecutionException("could not calculate  versions", e);
+		}
+
+	}
+
+}
